4de0c19e253c69399b036fa3a190aeca095a6711
Dan Addison <70022613+dan-shapeworks@users.noreply.github.com>
2022-11-19 12:31:54 +0000

    Add 'gig' CPT

--- wp-content/themes/dan-develops-wp/functions/content/custom_post_types.php
+++ wp-content/themes/dan-develops-wp/functions/content/custom_post_types.php
@@ -1,46 +1,46 @@
 <?php
 
 // Register Custom Post Type
-function register_project_custom_post_type() {
+function register_custom_post_types() {
 
+	// Gig
 	$labels = array(
-		'name'                  => 'Projects',
-		'singular_name'         => 'Project',
-		'menu_name'             => 'Portfolio',
-		'name_admin_bar'        => 'Portfolio',
-		'archives'              => 'Portfolio',
+		'name'                  => 'Gigs',
+		'singular_name'         => 'Gig',
+		'menu_name'             => 'Gigs',
+		'name_admin_bar'        => 'Gigs',
+		'archives'              => 'Gigs',
 		'attributes'            => 'Item Attributes',
 		'parent_item_colon'     => 'Parent Item:',
-		'all_items'             => 'All Projects',
-		'add_new_item'          => 'Add New Project',
-		'add_new'               => 'Add Project',
-		'new_item'              => 'New Project',
-		'edit_item'             => 'Edit Project',
-		'update_item'           => 'Update Project',
-		'view_item'             => 'View Project',
-		'view_items'            => 'View Projects',
-		'search_items'          => 'Search Projects',
-		'not_found'             => 'No projects found',
-		'not_found_in_trash'    => 'No projects found in Trash',
+		'all_items'             => 'All Gigs',
+		'add_new_item'          => 'Add New Gig',
+		'add_new'               => 'Add Gig',
+		'new_item'              => 'New Gig',
+		'edit_item'             => 'Edit Gig',
+		'update_item'           => 'Update Gig',
+		'view_item'             => 'View Gig',
+		'view_items'            => 'View Gigs',
+		'search_items'          => 'Search Gigs',
+		'not_found'             => 'No Gigs found',
+		'not_found_in_trash'    => 'No Gigs found in Trash',
 	);
 	$args = array(
-		'label'                 => 'Project',
+		'label'                 => 'Gig',
 		'labels'                => $labels,
-    'supports'              => array( 'title', 'editor', 'excerpt', 'thumbnail' ),
+		'supports'              => array( 'title', 'editor' ),
 		'hierarchical'          => false,
-    'public'                => true,
-    'capability_type'       => 'post',
+		'public'                => true,
 		'show_ui'               => true,
 		'show_in_menu'          => true,
 		'menu_position'         => 20,
-		'menu_icon'             => 'dashicons-format-gallery',
+		'menu_icon'             => 'dashicons-tickets-alt',
 		'show_in_admin_bar'     => true,
 		'show_in_nav_menus'     => true,
 		'can_export'            => true,
 		'has_archive'           => false,
 		'show_in_rest'          => true,
 	);
-	register_post_type( 'project', $args );
+	register_post_type( 'gig', $args );
 
 }
-add_action( 'init', 'register_project_custom_post_type', 0 );
+add_action( 'init', 'register_custom_post_types', 0 );
