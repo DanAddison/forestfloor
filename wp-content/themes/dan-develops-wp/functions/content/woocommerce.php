247163486d5d35fbc23c9bc353ec5176d69d68ea
Dan Addison <70022613+dan-shapeworks@users.noreply.github.com>
2022-12-28 15:32:51 +0000

    WC functions

--- wp-content/themes/dan-develops-wp/functions/content/woocommerce.php
+++ wp-content/themes/dan-develops-wp/functions/content/woocommerce.php
@@ -1,24 +1,4 @@
 <?php
-
-
-/**
- * Wrap WC archive/single page templates in our own default container
- */
-
-// // Opening div for our content wrapper
-// add_action('woocommerce_before_main_content', 'signal_open_div', 5);
- 
-// function signal_open_div() {
-//     echo '<div class="row row--pad row--woo">';
-// }
- 
-// // Closing div for our content wrapper
-// add_action('woocommerce_after_main_content', 'signal_close_div', 50);
- 
-// function signal_close_div() {
-//     echo '</div>';
-// }
-
 /**
  * Single product page
  */
@@ -49,12 +29,6 @@ function remove_short_description() {
 }
 add_action('add_meta_boxes', 'remove_short_description', 999);
 
-// add our own ACF field group section
-// add_action( 'woocommerce_single_product_summary', 'nebuloshift_single_product_specs', 30 );
-// function nebuloshift_single_product_specs() {
-// 	get_template_part( 'parts/single-product-specs' );
-// }
-
 /**
  * remove add to cart button on product archives
  */
@@ -63,18 +37,18 @@ remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_ad
 /*
 * Add 'how did you hear about us' feedback to checkout:
 */
-add_filter( 'woocommerce_checkout_fields', 'da_hear_about_us', 30 );
-function da_hear_about_us ( $fields ){
-	$fields['order']['hear_about_us'] = array(
-		'type' => 'select',
-		'label' => 'How did you hear about us?',
-		'options' => array(
-			'default' => '--select an option--',
-			'wom' => 'Word of mouth',
-			'google' => 'Google',
-			'social' => 'Social media',
-			'print' => 'Print'
-		)
-		);
-	return $fields;
-}
+// add_filter( 'woocommerce_checkout_fields', 'da_hear_about_us', 30 );
+// function da_hear_about_us ( $fields ){
+// 	$fields['order']['hear_about_us'] = array(
+// 		'type' => 'select',
+// 		'label' => 'How did you hear about us?',
+// 		'options' => array(
+// 			'default' => '--select an option--',
+// 			'instagram' => 'Instagram',
+// 			'youtube' => 'YouTube',
+// 			'print' => 'Print'
+// 			'wom' => 'Word of mouth',
+// 		)
+// 		);
+// 	return $fields;
+// }
