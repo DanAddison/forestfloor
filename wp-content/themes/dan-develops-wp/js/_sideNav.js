1b34ee98c47434b42ec8ef5ea44fc025677e83d1
Dan Addison <70022613+dan-shapeworks@users.noreply.github.com>
2023-01-02 14:00:59 +0000

    Delete unwanted stuff

--- wp-content/themes/dan-develops-wp/js/_sideNav.js
+++
@@ -1,8 +0,0 @@
-jQuery(function($) {
-	$(".menu-button").click(function(event) {
-	  event.preventDefault();
-	  $(".side-nav").toggleClass("menushow");
-	  $(".menu-button").toggleClass("is-active");
-	  $("body").toggleClass("is-visible-sidenav");
-	});
-});
